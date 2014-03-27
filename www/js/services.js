fb13105fe44cd07aca90c60f00d41701604909bc
Paul C Pederson <ppederson@esri.com>
2014-03-27 16:27:17 -0700

    realer activities log response and templating

--- www/js/services.js
+++ www/js/services.js
@@ -5,6 +5,7 @@ angular.module('starter.services', [])
  */
 .factory('Friends', function() {
 
+
   // Some fake testing data
   var friends = [
     { id: 0, name: 'Scruff McGruff' },
@@ -31,10 +32,10 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var activities = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
+    { id: 0, user: { name: 'Paul Pederson', username: 'paulcpederson', photo: 'https://avatars3.githubusercontent.com/u/1031758?s=460'}, amount: 2, date: '2014-03-27T18:00:00-0800', message: 'Hey man, cool stuff', latlng: [-120.277, 45.235]  },
+    { id: 1, user: { name: 'Nate Goldman', username: 'ngoldman', photo: 'https://avatars1.githubusercontent.com/u/427322?s=460'}, amount: -2, date: '2014-02-27T18:00:00-0800', message: 'better pay me back, yo', latlng: [-120.277, 45.235]  },
+    { id: 2, user: { name: 'Nikolas Wise', username: 'nikolaswise', photo: 'https://avatars2.githubusercontent.com/u/1987772?s=460'}, amount: 1, date: '2014-01-27T18:00:00-0800', message: 'thanks for the coffee', latlng: [-120.277, 45.235]  },
+    { id: 3, user: { name: 'Patrick Arlt', username: 'patrickarlt', photo: 'https://avatars1.githubusercontent.com/u/378557?s=460'}, amount: 2, date: '2013-12-14T18:00:00-0800', message: '', latlng: [-120.277, 45.235]  },
   ];
 
   return {
