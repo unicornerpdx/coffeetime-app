9782eec88fc790de664eb7181be4b1c7b345d1ba
Paul C Pederson <ppederson@esri.com>
2014-03-28 15:59:51 -0700

    change services to have more varied dates for testing

--- www/js/services.js
+++ www/js/services.js
@@ -9,7 +9,7 @@ angular.module('starter.services', [])
   var user = {
     name: 'Paul Pederson',
     username: 'paulcpederson',
-    amount: -12,
+    amount: -1,
     org: 'Esri PDX',
     photo: 'https://avatars3.githubusercontent.com/u/1031758?s=460'
   };
@@ -71,9 +71,9 @@ angular.module('starter.services', [])
   // Some fake testing data
   var activities = [
     { id: 0, user: { name: 'Paul Pederson', username: 'paulcpederson', photo: 'https://avatars3.githubusercontent.com/u/1031758?s=460'}, amount: 2, date: '2014-03-27T18:00:00-0800', message: 'Hey man, cool stuff', latlng: [-120.277, 45.235]  },
-    { id: 1, user: { name: 'Nate Goldman', username: 'ngoldman', photo: 'https://avatars1.githubusercontent.com/u/427322?s=460'}, amount: -2, date: '2014-02-27T18:00:00-0800', message: 'better pay me back, yo', latlng: [-120.277, 45.235]  },
-    { id: 2, user: { name: 'Nikolas Wise', username: 'nikolaswise', photo: 'https://avatars2.githubusercontent.com/u/1987772?s=460'}, amount: 1, date: '2014-01-27T18:00:00-0800', message: 'thanks for the coffee', latlng: [-120.277, 45.235]  },
-    { id: 3, user: { name: 'Patrick Arlt', username: 'patrickarlt', photo: 'https://avatars1.githubusercontent.com/u/378557?s=460'}, amount: 2, date: '2013-12-14T18:00:00-0800', message: '', latlng: [-120.277, 45.235]  },
+    { id: 1, user: { name: 'Nate Goldman', username: 'ngoldman', photo: 'https://avatars1.githubusercontent.com/u/427322?s=460'}, amount: -2, date: '2014-02-27T18:30:12-0800', message: 'better pay me back, yo', latlng: [-120.277, 45.235]  },
+    { id: 2, user: { name: 'Nikolas Wise', username: 'nikolaswise', photo: 'https://avatars2.githubusercontent.com/u/1987772?s=460'}, amount: 1, date: '2014-01-27T18:04:00-0800', message: 'thanks for the coffee', latlng: [-120.277, 45.235]  },
+    { id: 3, user: { name: 'Patrick Arlt', username: 'patrickarlt', photo: 'https://avatars1.githubusercontent.com/u/378557?s=460'}, amount: 2, date: '2013-12-14T14:00:00-0800', message: '', latlng: [-120.277, 45.235]  },
   ];
 
   return {
