d50a13fa00e25f2df3ff1001be73ad26f94cc9e8
Nikolas Wise <nikolaswise@gmail.com>
2014-03-28 17:24:17 -0700

    individual transaction view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,11 +11,8 @@ angular.module('starter.controllers', [])
 .controller('TransactionCtrl', function($scope, $stateParams, Group) {
   $scope.user = Group.get($stateParams.userID);
   $scope.number = 0;
-  $scope.increment = function(){
-    $scope.number++;
-  };
-  $scope.deincrement = function(){
-    $scope.number--;
+  $scope.increment = function(number){
+    $scope.number = $scope.number + number;
   };
 })
 
