d13867e835fb31c3f91cc0d40a4574634ce61fa2
Aaron Parecki <aaron@parecki.com>
2014-04-15 11:17:14 -0700

    remove old android build

--- platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/Users/patricklocal/code/coffee-web/platforms/android/ant-build/HelloCordova-debug-unaligned.apk : \
-/Users/patricklocal/code/coffee-web/platforms/android/ant-build/HelloCordova.ap_ \
-/Users/patricklocal/code/coffee-web/platforms/android/ant-build/classes.dex \
-/Users/patricklocal/code/coffee-web/platforms/android/CordovaLib/ant-build/classes.jar \
-/Users/patricklocal/code/coffee-web/platforms/android/libs/gcm.jar \
-/Users/patricklocal/code/coffee-web/platforms/android/libs/android-support-v13.jar \
