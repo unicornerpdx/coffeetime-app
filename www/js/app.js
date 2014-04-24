532440c0bed0ab3df09872989375f99b02766981
Patrick Arlt <patrick.arlt@gmail.com>
2014-04-24 10:33:31 -0700

    fix up loading flash, @TODOs

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,6 @@
-var openedWithURL;
+  var openedWithURL;
 
+// @TODO wrap this up in a service
 window.StatusBar = window.StatusBar || {
   styleLightContent: function(){
     console.log("LightStatusBar");
@@ -9,6 +10,7 @@ window.StatusBar = window.StatusBar || {
   }
 };
 
+// @TODO wrap this up in a service
 function handleOpenURL(url){
   // coffeetime://transaction?group_id=123&transaction_id=123
   var code = url.match(/code=(.+)/)[1];
@@ -17,11 +19,11 @@ function handleOpenURL(url){
   });
 }
 
+// @TODO wrap this up in a service
 function onNotificationGCM(e){
   console.log(e);
   switch( e.event ){
   case 'registered':
-    console.log("regID = " + e.regid);
     angular.element(document.body).injector().invoke(function(Session){
       Session.registerAndroidToken(e.regid);
     });
@@ -51,6 +53,7 @@ function onNotificationGCM(e){
   }
 }
 
+// @TODO wrap this up in a service
 function onNotificationAPN(e){
   angular.element(document.body).injector().invoke(function($state){
     $state.go("tab.activity-detail", {
@@ -63,13 +66,6 @@ function onNotificationAPN(e){
   }
 }
 
-// Ionic Starter App, v0.9.20
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'app.filters', 'app.directives', 'angularMoment'])
 
 .value("Config", {
@@ -82,7 +78,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 
     window.StatusBar.styleLightContent();
 
-    // Register for push here if user is logged in.
     if(Cache.me()){
       Session.sendPushTokens();
     }
@@ -102,15 +97,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  var store = new TinyStore('coffee');
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
-    // setup an abstract state for the tabs directive
     .state('tab', {
       url: "/tab",
       abstract: true,
@@ -173,7 +161,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 })
 
 .run(function($state, Cache){
-    // if none of the above states are matched, use this as the fallback
   if(Cache.token()){
     $state.go('tab.me');
   } else {
